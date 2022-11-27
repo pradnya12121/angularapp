6e686c3ed1971397659ad523a0de1b712d98c3bf
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-11-27 17:46:09 +0530

    27_11_update
    27_11_update

--- src/app/ngswitch/ngswitch.component.ts
+++ src/app/ngswitch/ngswitch.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngswitch.component.css']
 })
 export class NgswitchComponent implements OnInit {
-
+selectedProduct:string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  getSelectedValue(value){
+ console.log(value.target.value);
+ this.selectedProduct=value.target.value;
+  }
 }
