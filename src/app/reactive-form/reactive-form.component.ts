8c2b6a25ff0f1b52a8a2aacc30dbef500b4299f5
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-12-13 18:39:02 +0530

    13_12_update
    13_12_update

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -8,13 +8,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
 myReactiveForm:FormGroup;
+genders:any[]=[{id:'1',value:'Female'},{id:'2',value:'Male'}]
+notAllowedNames=['codemind','technology']
   constructor() { 
 this.createForm();
   }
 createForm(){
   this.myReactiveForm=new FormGroup({
-    'username':new FormControl('',Validators.required),
-    'email':new FormControl('',Validators.required)
+    'username':new FormControl('',[Validators.required,this.NaName.bind(this)]),
+    'email':new FormControl('',[Validators.required,Validators.email]),
+    'course':new FormControl('Angular'),
+    'gender':new FormControl('')
   })
 }
 onSubmit(){
@@ -22,5 +26,10 @@ onSubmit(){
 }
   ngOnInit() {
   }
-
+NaName(controls:FormControl){
+if(this.notAllowedNames.indexOf(controls.value)!== -1){
+return{'nameNotAllowed':true}
+}
+return null;
+}
 }
