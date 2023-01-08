c518a64841bff046de7d36f58dd2b2f9bd8dd14c
pradnya12121 <pradnya.kolase1994@gmail.com>
2023-01-08 15:54:52 +0530

    update practice
    update practice

--- src/app/re-p/re-p.component.ts
+++ src/app/re-p/re-p.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-re-p',
@@ -10,20 +11,47 @@ export class RePComponent implements OnInit {
 myForm:FormGroup;
 array:any[]=[{id:1,value:'Female'},
 {id:2,value:'Male'}]
+nameNotAlloed=['codemind','technology']
   constructor() { 
     this.createForm();
   }
 
   ngOnInit() {
+    // setTimeout(()=>{
+    //   this.myForm.setValue({
+    //     'username':'Pradnya',
+    //     'email' :'pradnya.kolase1994@gmail.com',
+    //     'gender':'Female',
+    //     'course':'Angular',
+    //     'password':'123',
+    //     'confirmPassword':'123'
+    //   })
+    // },2000)
+    // this.myForm.patchValue({
+    //   'username':'Pradnya'
+    // })
+    
   }
   createForm(){
     this.myForm=new FormGroup({
-     'username':new FormControl('',[Validators.required]),
-     'email' : new FormControl('',[Validators.required]),
-     'gender': new FormControl(''),
-     'course': new FormControl('Angular')
+     'username':new FormControl('',[Validators.required,Validators.maxLength(10), this.nName.bind(this)]),
+     'email' : new FormControl('',[Validators.required],this.Nemails),
+     'gender': new FormControl('Female'),
+     'course': new FormControl('Angular'),
+     'password':new FormControl('',[Validators.required,Validators.min(10)]),
+     'confirmPassword': new FormControl('',[Validators.required,Validators.min(10)]),
+     'skills':new FormArray([
+      new FormControl(null,Validators.required)
+     ])
 
     })
+  }
+  nName(controls:FormControl){
+    if(this.nameNotAlloed.indexOf(controls.value)!==-1){
+      return {'nameNotAllowed':true}
+    }
+    return null;
+
   }
   get username(){
     return this.myForm.get('username')
@@ -32,4 +60,32 @@ array:any[]=[{id:1,value:'Female'},
   get email(){
     return this.myForm.get('email')
   }
+  get gender(){
+    return this.myForm.get('gender')
+  }
+  get password(){
+    return this.myForm.get('password') }
+ get confirmPassword(){
+   return this.myForm.get('confirmPassword')
+  }
+  onSubmit(){
+    console.log(this.myForm.value)
+  }
+  Nemails(controls:FormControl):Promise <any> | Observable <any>{
+    const myPromise= new Promise((resolve,reject)=>{
+      if(controls.value === 'codemind@gmail.com'){
+       resolve({'emailNot':true})
+      }
+      else {
+       resolve(null)
+      }
+
+    })
+    return myPromise;
+  }
+
+  // addSkills(){
+  //   (<FormArray>this.myForm.get('skills').push(new FormControl(null,Validators.required)))
+  // }
 }
+
