52faaf8802df708df97222aeacea31a0833554e6
pradnya12121 <pradnya.kolase1994@gmail.com>
2023-01-27 21:49:12 +0530

    update
    update

--- src/app/filtercomponent/filtercomponent.component.ts
+++ src/app/filtercomponent/filtercomponent.component.ts
@@ -11,7 +11,8 @@ nameSearch:string='';
   product:any[]=[{sno:1,name:'Mobile',price:20000,availability:'Available'},
   {sno:2,name:'TV',price:20000,availability:'Available'},
   {sno:3,name:'Tab',price:30000,availability:'Available'},
-  {sno:4,name:'Washing Machine',price:60000,availability:'Not-Available'},]
+  {sno:4,name:'Washing Machine',price:60000,availability:'Not-Available'},
+  {sno:4,name:'Laptop',price:60000,availability:'Not-Available'}]
 
   ngOnInit() {
   }
